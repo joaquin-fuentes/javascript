37807b25103c115299d3a3660c4de673e6a3ecb9
Joaquin Fuentes <joaquin.fuentes1327@gmail.com>
2025-05-26 21:26:36 -0300

    Clase de variables, tipos de datos y operadores

---
+++ variables/app.js
@@ -0,0 +1,25 @@
+// let nombre = prompt("Ingrese su nombre");
+// const edad = prompt("Ingrese su edad");
+
+// console.log("Hola, mi nombre es", nombre, "y mi edad es:", edad);
+
+// TIPOS DE DATOS
+// string
+// let nombre = "Joaquin fuentes";
+// // number
+// let edad = 31;
+// //bolleano
+// let mayorDeEdad = false;
+// //undefined
+// let apellido;
+// // null
+// let cualquiercosa = null;
+
+// OPERADORES
+
+let numeroA = 5;
+let numeroB = 10;
+
+let resultado = numeroA / numeroB;
+
+console.log(resultado);
