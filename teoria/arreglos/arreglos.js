3e56db00aad498b89a29f074bbb8acd02ab0b679
Joaquin Fuentes <joaquin.fuentes@rollingcodestudio.com>
2025-06-09 20:13:04 -0300

    primera parte clase de arrays

---
+++ teoria/arreglos/arreglos.js
@@ -0,0 +1,18 @@
+let frutas = ["manzana", "banana", "naranja"];
+
+// mostrar valor
+// console.log(frutas[2]);
+// modificar un valor
+
+frutas[1] = "pera";
+
+// tamaño de un arreglo
+// console.log(frutas.length);
+
+// agregar un elemento
+frutas.push("sandia");
+// console.log(frutas);
+
+for (let i = 0; i < frutas.length; i++) {
+  console.log(frutas[i]);
+}
