48df3ddc213af3824a69a41991b628f7ee8bcaf9
Joaquin Fuentes <joaquin.fuentes1327@gmail.com>
2025-06-30 21:26:20 -0300

    segunda parte de practica de CRUD, CRUD completo

--- teoria/CRUD-PELIS/crudPelis.js
+++ teoria/CRUD-PELIS/crudPelis.js
@@ -1,6 +1,29 @@
 let peliculas = [];
+recuperarPeliculasDesdeLocalStorage();
+agregarPeliculasDeEjemploSiEsNecesario();
+
+function guardarEnLocalStorage() {
+  const peliculasJSON = JSON.stringify(peliculas);
+  localStorage.setItem("peliculas", peliculasJSON);
+}
+function recuperarPeliculasDesdeLocalStorage() {
+  const peliculasRecuperadas = localStorage.getItem("peliculas");
+  if (peliculasRecuperadas) {
+    peliculas = JSON.parse(peliculasRecuperadas);
+  } else {
+    peliculas = [];
+  }
+}
+function agregarPeliculasDeEjemploSiEsNecesario() {
+  if (peliculas.length === 0) {
+    agregarPeli("Harry Potter", "Felipe Marrone", 2006, 8.3);
+    agregarPeli("Capitán América", "Pablo Torres", 2012, 5);
+  }
+}
 // CREATE
 function agregarPeli(titulo, director, anio, calificacion) {
+  // recuperar antes de modificar para evitar sobrescribir
+  recuperarPeliculasDesdeLocalStorage();
   // crear la nueva pelicula
   const nuevaPelicula = {
     id: Date.now(),
@@ -16,29 +39,74 @@ function agregarPeli(titulo, director, anio, calificacion) {
   };
   // agregar la nueva pelicula al listado de peliculas
   peliculas.push(nuevaPelicula);
+  guardarEnLocalStorage();
   console.log("✅ Película agregada:", nuevaPelicula);
 }
-agregarPeli("Harry Potter", "Felipe Marrone", 2006, 8.3);
-agregarPeli("Capitan America", "Pablo Torres", 2012, 5);
 
 // READ
-function listarPelis() {
+function listarPelis(pelis) {
+  recuperarPeliculasDesdeLocalStorage();
   console.log("🎬 Lista de películas:");
-  peliculas.forEach((pelicula, index) => {
+  pelis.forEach((pelicula, index) => {
     const { titulo, director, anio, calificacion } = pelicula;
     console.log(
       `${index + 1} - ${titulo}, ${director}, ${anio}, ${calificacion}`
     );
   });
 }
-
 function buscarPeliPorTitulo(nombrePeli) {
+  recuperarPeliculasDesdeLocalStorage();
   const resultados = peliculas.filter((pelicula) =>
     pelicula.titulo.toLowerCase().includes(nombrePeli.toLowerCase())
   );
   console.log("🔎 Resultados de búsqueda:");
-  console.log(resultados);
+  listarPelis(resultados);
 }
 
-listarPelis();
-buscarPeliPorTitulo("capi");
+function actualizarCalificacion() {
+  const id = parseInt(
+    prompt("Ingrese el id de la pelicula que quiere actualizar")
+  );
+  const nuevaCalificacion = parseFloat(
+    prompt("Ingrese la nueva calificación de la pelicula.")
+  );
+  let encontrada = false;
+  peliculas = peliculas.map((peli) => {
+    if (peli.id === id) {
+      encontrada = true;
+      return { ...peli, calificacion: nuevaCalificacion };
+    }
+    return peli;
+  });
+  if (encontrada) {
+    guardarEnLocalStorage();
+    alert("✅ Calificación actualizada.");
+  } else {
+    alert("❌ Película no encontrada con ese ID.");
+  }
+}
+
+function eliminarPelicula() {
+  const id = parseInt(
+    prompt("Ingrese el id de la pelicula que quiere ELIMINAR")
+  );
+  const index = peliculas.findIndex((peli) => peli.id === id);
+  if (index !== -1) {
+    const confirmacion = confirm("Seguro que desea eliminar esta pelicula?");
+    if (confirmacion) {
+      //  encontro
+      const eliminada = peliculas.splice(index, 1)[0];
+      guardarEnLocalStorage();
+      alert(`🗑️ Película eliminada: ${eliminada.titulo}`);
+    }
+  } else {
+    // no lo encontro
+    alert("❌ Película no encontrada.");
+  }
+}
+eliminarPelicula();
+
+// Realizar el switch para preguntar al usuario
+// que accion desea realizar y
+// mostrar por consola lo que el usuario eligio
+function mostrarMenu() {}
